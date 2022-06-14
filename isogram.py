383d8b0bcbc22fb4eb21de29cb7f7c96a13d5f12
Berke Yay <105660365+Berke-Yay@users.noreply.github.com>
2022-06-14 17:01:49 +0300

    Update isogram.py

--- isogram.py
+++ isogram.py
@@ -1,9 +1,8 @@
 def is_isogram(string):
-    #your code here
+    #This function checks if the given string consists of distinctive characters.
 
     string = string.lower()
     for i in string:
         if string.count(i)>1:
             return False
     return True
-is_isogram("")
