4899460dba39f5da9eea83e864b678984932be6c
Berke Yay <105660365+Berke-Yay@users.noreply.github.com>
2022-06-14 17:00:24 +0300

    Update count_bits.py

--- count_bits.py
+++ count_bits.py
@@ -1,7 +1,7 @@
+#This function returns the number of 1's in the binary value of an integer.
 def count_bits(n):
     binary =[]
     while n!=1:
         binary.append(n%2)
         n=n//2
     return binary.count(1)+1
-print(count_bits(50))
